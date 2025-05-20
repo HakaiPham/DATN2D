86a79bf2ee840885e38971ae478d58096c796db0
KhanGsGodXs <khang200575@gmail.com>
2025-05-20 17:43:46 +0700

    added multiple folder

---
+++ Assets/Script/NPC/UpgradeNPC.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class UpgradeNPC : MonoBehaviour
+{
+    public GameObject upgradeUI; // Giao diện nâng cấp cần bật/tắt
+    public KeyCode interactKey = KeyCode.E; // Phím tương tác
+
+    private bool playerInRange = false;
+
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(interactKey))
+        {
+            if (upgradeUI != null)
+            {
+                bool isActive = upgradeUI.activeSelf;
+                upgradeUI.SetActive(!isActive);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            // Bạn có thể hiển thị "Nhấn E để nâng cấp" ở đây nếu muốn
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+
+            // Tự động tắt UI khi rời khỏi vùng NPC
+            if (upgradeUI != null && upgradeUI.activeSelf)
+            {
+                upgradeUI.SetActive(false);
+            }
+        }
+    }
+}
