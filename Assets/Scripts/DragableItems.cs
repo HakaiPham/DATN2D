9b3cbfac4700b0f92fecf56f6e345fad2c6bbf38
DeadInside1213 <avenerairline@gmail.com>
2025-05-27 21:46:42 +0700

    Save1

--- Assets/Scripts/DragableItems.cs
+++ Assets/Scripts/DragableItems.cs
@@ -3,18 +3,26 @@ using UnityEngine.EventSystems;
 
 public class DragableItems : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    Transform parentAfterDrag;
+
+
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("Begin Drag");
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         Debug.Log("Dragging");
+        transform.position = Input.mousePosition;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("End Drag");
+        transform.SetParent(parentAfterDrag);
     }
 }
