a7626652bb1709caff58a2a439f34b33642b498c
Ling Qin Ru Xue <Hatsuneyato11@gmail.com>
2025-06-01 20:49:29 +0700

    ShopV1

---
+++ Assets/Resources/ResourcesShop/ShopCode/ButtonShop.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems; // Để dùng IPointerClickHandler
+
+public class ButtonShop : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject menuToShow; // Kéo menu UI vào đây trong Inspector
+
+    // Hàm sẽ tự động được gọi khi ảnh được click
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (menuToShow != null)
+        {
+            // Bật / tắt menu mỗi lần click
+            menuToShow.SetActive(!menuToShow.activeSelf);
+        }
+    }
+    public void CloseShop()
+    {
+        if (menuToShow != null)
+        {
+            menuToShow.SetActive(false); // Tắt menu
+        }
+    }
+
+}
